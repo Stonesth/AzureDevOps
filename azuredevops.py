ec09a123c899c5c391fbfc5e2af9f7f3dcd93d12
Stonesth <pierre.thonon@gmail.com>
2025-12-01 10:29:22 +0100

    Increase delay before selecting Product Backlog Item in createNewPBI function

--- azuredevops.py
+++ azuredevops.py
@@ -199,7 +199,7 @@ def createNewPBI(iteration, sprint, caller, incidentTitle, description_text) :
     # Need to select the Product Backlog Item
     # Not possible to find directly the xpath of the Product Backlog Item
     # So we need to use the keyboard to select the Product Backlog Item, press the tab 1 time and then press the down arrow 1 time
-    time.sleep(1)
+    time.sleep(10)
     button.send_keys(Keys.ARROW_DOWN)
     button.send_keys(Keys.ARROW_DOWN)
     time.sleep(10)
