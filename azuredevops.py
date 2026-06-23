11d607673a3d14dd82805dc30bc82dfbdee3eed9
Stonesth <pierre.thonon@gmail.com>
2026-06-23 12:05:02 +0200

    Fix createNewPBI function to correct iteration field input formatting

--- azuredevops.py
+++ azuredevops.py
@@ -277,7 +277,8 @@ def createNewPBI(iteration, sprint, caller, incidentTitle, description_text) :
 
     # past the iteration + sprint
     year = iteration.split('.')[0]
-    iteration_field.send_keys("Finance\\" + year + "\\PI" + iteration +  "\\PI" + iteration + "." + sprint)
+    # iteration_field.send_keys("Finance\\" + "\\PI" + iteration +  "\\PI" + iteration + "." + sprint)
+    iteration_field.send_keys("Finance\\PI" + iteration +  "\\PI" + iteration + "." + sprint)
     time.sleep(1)
     iteration_field.send_keys(Keys.ENTER)
     time.sleep(1)
